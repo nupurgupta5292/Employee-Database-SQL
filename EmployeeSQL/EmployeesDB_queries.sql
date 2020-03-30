f617e77d41f36be282efa8db7cd7cc17a21ffa48
nupurgupta5292 <nupur.gupta5292@gmail.com>
2020-03-30 19:51:45 -0400

    Added sql files and images

---
+++ EmployeeSQL/EmployeesDB_queries.sql
@@ -0,0 +1,62 @@
+-- Checking to see if tables have been imported successfully
+SELECT * FROM departments LIMIT 10
+SELECT * FROM dept_emp LIMIT 10
+SELECT * FROM dept_manager LIMIT 10
+SELECT * FROM employees LIMIT 10
+SELECT * FROM salaries LIMIT 10
+SELECT * FROM titles LIMIT 10
+
+-- Queries
+-- List the following details of each employee: employee number, last name, first name, gender, and salary
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
+FROM employees AS e
+LEFT JOIN salaries AS s ON e.emp_no = s.emp_no
+
+-- List employees who were hired in 1986
+SELECT * FROM employees WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates
+SELECT m.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name, m.from_date, m.to_date
+FROM dept_manager AS m
+INNER JOIN departments AS d
+ON m.dept_no = d.dept_no
+INNER JOIN employees AS e
+ON e.emp_no = m.emp_no;
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp
+ON e.emp_no = dept_emp.emp_no
+INNER JOIN departments AS d
+ON dept_emp.dept_no = d.dept_no
+
+-- List all employees whose first name is "Hercules" and last names begin with "B."
+SELECT * FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp
+ON e.emp_no = dept_emp.emp_no
+INNER JOIN departments AS d
+ON dept_emp.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+INNER JOIN dept_emp
+ON e.emp_no = dept_emp.emp_no
+INNER JOIN departments AS d
+ON dept_emp.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+OR d.dept_name = 'Development';
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
+SELECT last_name, COUNT(last_name) AS "Last Name Frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY "Last Name Frequency" DESC
---
+++ EmployeeSQL/EmployeesDB_table-schemata.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS departments CASCADE
+DROP TABLE IF EXISTS dept_emp CASCADE
+DROP TABLE IF EXISTS dept_manager CASCADE
+DROP TABLE IF EXISTS employees CASCADE
+DROP TABLE IF EXISTS salaries CASCADE
+DROP TABLE IF EXISTS titles CASCADE
+
+CREATE TABLE departments (
+dept_no VARCHAR PRIMARY KEY,
+dept_name VARCHAR
+);
+
+CREATE TABLE employees (
+emp_no INTEGER PRIMARY KEY,
+birth_date DATE,
+first_name VARCHAR,
+last_name VARCHAR,
+gender VARCHAR,
+hire_date DATE
+);
+
+CREATE TABLE dept_emp (
+emp_no INTEGER,
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+dept_no VARCHAR,
+FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+from_date DATE,
+to_date DATE
+);
+
+CREATE TABLE dept_manager(
+dept_no VARCHAR,
+FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+emp_no INTEGER PRIMARY KEY,
+FOREIGN KEY (emp_no) REFERENCES dept_emp(emp_no),
+from_date DATE,
+to_date DATE
+);
+
+CREATE TABLE salaries(
+emp_no INTEGER PRIMARY KEY,
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+salary INTEGER,
+from_date DATE,
+to_date DATE
+);
+
+CREATE TABLE titles(
+emp_no INTEGER,
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+title VARCHAR,
+from_date DATE,
+to_date DATE
+);
